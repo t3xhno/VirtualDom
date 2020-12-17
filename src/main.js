7b4adc97b28b813f32e399945e0b8aa3c0fe19da
t3xhno <t3xhnolainx@gmail.com>
2020-12-17 17:16:33 +0100

    Add interval update of whole app

--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
 import createELement from './vdom/createElement';
+import render from './vdom/render';
+import mount from './vdom/mount';
 
-const VApp = createELement("div", {
+const createVApp = (count) => createELement("div", {
     attrs: {
         id: "app",
+        dataCount: count
     },
-    children: [],
+    children: [
+        String(count),
+        createELement("img", {
+            attrs: {
+                src: "https://media.giphy.com/media/JpwS1kUPG0mpimj9fz/giphy.gif"
+            },
+        })
+    ],
 });
 
-console.log(VApp);
+let count = 0;
+const vApp = createVApp(count);
+
+const $app = render(vApp);
+
+let $rootEl = mount($app, document.getElementById("app"));
+
+setInterval(() => {
+    count++;
+    $rootEl = mount(render(createVApp(count)), $rootEl);
+}, 1000);
+
+console.log($app);
